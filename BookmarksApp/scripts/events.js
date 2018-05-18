d6757890c6599ce9e448747a9cc804179b80dffb
Aram Hammoudeh <aramcontact@gmail.com>
2018-05-18 13:24:38 -0600

    Delete function working, creating expansion function

--- BookmarksApp/scripts/events.js
+++ BookmarksApp/scripts/events.js
@@ -8,13 +8,19 @@ const events = (function () {
     api.getItems(function (listed) {
       store.storedBookmarks = listed;
       for (var i = 0; i < store.storedBookmarks.length; i++) {
-        $('#condensed').remove();
+        $('.condensed').remove();
         $('.delete').remove();
       }
+
+
+
+      events.deleteBookmark();
+
+
       for (var y = 0; y < store.storedBookmarks.length; y++) {
         console.log('click');
         $('#sidebar').append(`
-      <button id="condensed" class="w3-bar-item w3-button ">(${store.storedBookmarks[y].rating})${store.storedBookmarks[y].title}<span><button id="${store.storedBookmarks[y].id}" class="delete w3-button">X</button></span></button>
+      <button id="${store.storedBookmarks[y].id}" class=" condensed w3-bar-item w3-button ">(${store.storedBookmarks[y].rating})${store.storedBookmarks[y].title}<button id="${store.storedBookmarks[y].id}" class="delete w3-button">X</button></button>
       `);
       }
     });
@@ -25,22 +31,62 @@ const events = (function () {
     console.log('render ran');
 
   };
+  // function handleExpand() {
+  //   //for (let i = 0; i < store.storedBookmarks.length; i++) {
+  //   $(`#${store.storedBookmarks[i].id}`).click(function () {
+  //     expand.displayOnExpand(store.storedBookmarks[i].title, store.storedBookmarks[i].url, store.storedBookmarks[i].description, store.storedBookmarks[i].rating);
+  //   });
 
-  function removeBookmark() {
-    console.log('delete live');
+  //   //}
 
-    let targetId = 1;
-    $.ajax({
-      url: 'https://thinkful-list-api.herokuapp.com/ara/bookmarks/' + targetId,
-      type: 'DELETE',
-      success: function (result) {
-        console.log('delete successful');
-        events.render();
-        // Do something with the result
+
+
+  // }
+  // handleExpand();
+  function deleteBookmark() {
+    console.log('check');
+    api.getItems(function () {
+      function getID(val) {
+        let marks = store.storedBookmarks;
+        return marks[val].id;
       }
+
+
+      console.log('delete live');
+      $('.delete').click(function () {
+        console.log('pressed');
+        let targetId = $('.delete').attr('id');
+        $.ajax({
+          url: 'https://thinkful-list-api.herokuapp.com/ara/bookmarks/' + targetId,
+          type: 'DELETE',
+          success: function (result) {
+            console.log('delete successful');
+            $(`#${targetId}`).remove();
+            $(`#${targetId}`).remove();
+            // Do something with the result
+          }
+        });
+        events.render();
+      });
+
+
     });
 
+
   }
+
+
+
+  // function filterOut() {
+  //   $('.filter').click(function () {
+  //     for (let i = 0; i < array.length; i++) {
+
+
+  //     }
+  //   });
+  // }
+
+
   function handleAddNewBookmark() {
     $('#add').click(function () {
       $('#log').remove();
@@ -48,16 +94,7 @@ const events = (function () {
     });
   }
 
-  function handleExpand() {
-    //TEST CALLS
-    $('#button-one').click(function () {
-      expand.displayOnExpand('title', 'https://thinkful.com', 'This is a description of the title', '4');
-    });
 
-    $('#button-two').click(function () {
-      expand.displayOnExpand('title2', 'https://thinkful.com', 'This is a description of the title', '4');
-    });
-  }
 
   // function handlePullFromForm() {
   //   console.log('Can pull');
@@ -72,8 +109,8 @@ const events = (function () {
 
   const bindEventListener = function () {
     handleAddNewBookmark();
-    removeBookmark();
-    handleExpand();
+    //removeBookmark();
+    //handleExpand();
     //handlePullFromForm();
     console.log('event listener live');
   };
@@ -81,6 +118,7 @@ const events = (function () {
   return {
     bindEventListener,
     render,
+    deleteBookmark,
 
   };
 })();
